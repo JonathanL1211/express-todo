24a99110f3b70cd3b313723abe5eb844e64891f5
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-29 12:39:33 +0800

    modify: futher 2 => prompt user not to leave empty inputs for the form when submitting

--- views/homepage.jsx
+++ views/homepage.jsx
@@ -4,20 +4,19 @@ var DefaultLayout = require('./layout/defaulthome');
 
 class Home extends React.Component {
   render() {
-    let homepage = "homepage";
     //console.log(this.props);
     return (
         <DefaultLayout title="Home Page">
             <div>
                 <h1>Things to do in your life: </h1>
-                <form method="POST" action="/todo">
+                <form id="form" method="POST" action="/todo" >
                     <p>Tasks: </p>
-                    <input type="text" name="task"/>
+                    <input type="text" name="task" required/>
                     <p>Days to complete: </p>
-                    <input type="text" name="days"/>
+                    <input type="text" name="days" required/>
                     <p>Status: </p>
-                    <select name="status">
-                    <option value="select">Select one option...</option>
+                    <select name="status" required>
+                    <option value="">Select one option...</option>
                     <option value="completed">Completed</option>
                     <option value="inProgress">In-progress</option>
                     <option value="haventStart">Haven't Start</option>
