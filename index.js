458bdce5370ce3d53e96891eab0f8ffcc9508bc4
JonathanL1211 <Jonathanknight1211@gmail.com>
2018-09-29 10:36:28 +0800

    add: POST method to insert todo-list

--- index.js
+++ index.js
@@ -23,9 +23,38 @@ app.set('views', __dirname + '/views');
 // this line sets react to be the default view engine
 app.set('view engine', 'jsx');
 
+//add JSON file
+const FILE = 'shoppinglist.json';
+//-----------------------------------------------------------------------------------------------------------
+//express request
 app.get('/', (req, res) =>{
     res.render('homepage');
 })
+
+app.post('/todo', (req, res) =>{
+    //console.log(req.body);
+    var reqOrd = req.body;
+    //const obj = req.body;
+    let tasksObj = {
+        task: reqOrd['task'],
+        daysCompletion: parseInt(reqOrd['days']),
+        status: reqOrd['status']
+    }
+
+    jsonfile.readFile(FILE, (err,obj)=>{
+        obj['task'].push(tasksObj);
+        var listObj = obj['task'];
+        //console.log(obj);
+
+        jsonfile.writeFile(FILE, obj, function(err){
+            if (err){
+                console.log("ERROR: ", err);
+            }
+            res.render('todolist', {task: listObj});
+        });
+    })
+})
+
 
 
 
